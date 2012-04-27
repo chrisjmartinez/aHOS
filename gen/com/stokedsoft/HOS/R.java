3817f5cd94d7a6b101eed072366726516ed89842
Chris Martinez <chrisjmartinez@comcast.net>
2012-04-26 21:10:22 -0400

    Cosmetic string changes

--- gen/com/stokedsoft/HOS/R.java
+++ gen/com/stokedsoft/HOS/R.java
@@ -27,11 +27,10 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int LatLabel=0x7f040003;
-        public static final int LonLabel=0x7f040004;
-        public static final int UserLabel=0x7f040002;
-        public static final int app_name=0x7f040001;
-        public static final int cacheLimit=0x7f040005;
-        public static final int hello=0x7f040000;
+        public static final int LatLabel=0x7f040002;
+        public static final int LonLabel=0x7f040003;
+        public static final int UserLabel=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int cacheLimit=0x7f040004;
     }
 }
